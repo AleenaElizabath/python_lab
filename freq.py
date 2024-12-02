73a8ba3ea3aed46036ec777561cd074ef8605c6d
AleenaElizabath <AleenaElizabathmanoj@gmail.com>
2024-12-02 11:57:01 +0530

    freq correction

--- freq.py
+++ freq.py
@@ -1,4 +1,10 @@
-string=input("Enter a string:")
+string = input("Enter a string: ")
+
+# Create an empty set to track characters we've already processed
+processed = set()
+
 for i in string:
-    freq= string.count(i)
-    print(str(i) + ": " + str(freq), end=", ")
+    if i not in processed:
+        freq = string.count(i)
+        print(str(i) + ": " + str(freq), end=", ")
+        processed.add(i)
